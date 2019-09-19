e7da463c9be9035924b7e50ed1697ca6cdd0f09f
Jennifer <jennifertencate@Jennifers-MacBook-Pro.local>
2019-09-19 12:33:43 -0500

    Updates

---
+++ precache-manifest.07ffb892d697559b2248fbbb62518d38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e59ddadae74eec0a9cd695c17ab764",
+    "url": "/perpetualmotion/index.html"
+  },
+  {
+    "revision": "d2cefdc924c43fd511b7",
+    "url": "/perpetualmotion/static/css/main.338ab6d0.chunk.css"
+  },
+  {
+    "revision": "0737cdfb2d38484b47f3",
+    "url": "/perpetualmotion/static/js/2.5efea958.chunk.js"
+  },
+  {
+    "revision": "d2cefdc924c43fd511b7",
+    "url": "/perpetualmotion/static/js/main.2a7d3efa.chunk.js"
+  },
+  {
+    "revision": "956c70c4ed04eac13607",
+    "url": "/perpetualmotion/static/js/runtime~main.6e646e74.js"
+  }
+]);
